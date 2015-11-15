6c94e56071743b04eaada9bed8dab4a932b665e8
KeithC <keithcc1966@gmail.com>
2015-11-15 10:26:56 -0500

    switch from nose to unittests

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ config = {
     'download_url': 'Where to download it.',
     'author_email': 'keithcc1966@gmail.com',
     'version': '0.1',
-    'install_requires': ['nose'],
+    'install_requires': [],
     'packages': ['python_example_package'],
     'scripts': [],
     'name': 'python_example_package'
--- tests/python_example_package_tests.py
+++ tests/python_example_package_tests.py
@@ -1,16 +1,20 @@
-from nose.tools import *
+# Run these tests from ipython in the main package directory:
+# `run tests\python_example_package_tests.py`
+import unittest
 import python_example_package
 
-def setup():
-    print "SETUP!"
-
-def teardown():
-    print "TEAR DOWN!"
-
-def test_basic():
-    print "I RAN!"
     
-def test_add():
-    assert_equal( python_example_package.add(1,2),  3)
-    assert_equal( python_example_package.add(0,0),  0)
-    assert_equal( python_example_package.add(-1,-1),  -2)
+class TestAdd(unittest.TestCase):
+
+    def test_basic(self):
+        print "I RAN!"
+        
+    def test_add(self):        
+        self.assertEqual( python_example_package.add(1,2),  3)
+        self.assertEqual( python_example_package.add(0,0),  0)
+        self.assertEqual( python_example_package.add(-1,-1),  -2)
+
+
+if __name__=='__main__':
+    print python_example_package.add(1,2)
+    unittest.main()	
